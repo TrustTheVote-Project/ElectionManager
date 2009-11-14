37d3f36b472470f3fafbd3e88ef82bcb835663c1
Aleks Totic <a@totic.org>
2009-11-13 17:17:03 -0800

    write in, turn pages in header/footer

--- lib/ttv/pdf_ballot.rb
+++ lib/ttv/pdf_ballot.rb
@@ -1,8 +1,9 @@
+
 require '../../test/test_helper'
 
 require 'prawn'
+require 'prawn/format'
 
-#if false
 class PDFBallotTest < ActiveSupport::TestCase
   def test_GenerateBallot
     election = Election.find(:first)
@@ -16,7 +17,6 @@ class PDFBallotTest < ActiveSupport::TestCase
     assert_not_nil(precinct)
   end
 end
-#end
 
 module TTV
   module PDFBallot
@@ -40,8 +40,8 @@ module TTV
       end
 
       def inset(horiz, vertical)
-        @top += vertical
-        @bottom -= vertical
+        @top -= vertical
+        @bottom += vertical
         @left += horiz
         @right -= horiz
       end
@@ -77,29 +77,35 @@ module TTV
       end
 
       def draw_into(config, rect)
+        top = rect.top
         config.pdf.font("Helvetica", :size => 10, :style => :italic)
         config.pdf.bounding_box([rect.left + 2, rect.top], :width => rect.width - 2 ) do
           config.pdf.move_down 3
           config.pdf.text "FlowItem.draw_into"
           rect.top -= config.pdf.bounds.height
         end
-        config.pdf.stroke_line([rect.left, rect.top], [rect.right, rect.top])
+        config.pdf.stroke_line [rect.left, rect.top], [rect.right, rect.top]
+        config.pdf.stroke_line [rect.right, rect.top], [rect.right, top]
       end
 
       class Header < FlowItem
         def draw_into(config, rect)
-          config.pdf.font("Helvetica", :size => 10)
+          top = rect.top
+          config.pdf.font("Helvetica", :size => 10, :style => :bold )
           config.pdf.bounding_box([rect.left + 2, rect.top], :width => rect.width - 2) do
             config.pdf.move_down 3
             config.pdf.text @item, :leading => 1
             rect.top -= config.pdf.bounds.height 
           end
+          config.pdf.line_width 0.5
           config.pdf.stroke_line([rect.left, rect.top], [rect.right, rect.top])
+          config.pdf.stroke_line [rect.right, rect.top], [rect.right, top]
         end
       end
 
       class Question < FlowItem
         def draw_into(config, rect)
+          top = rect.top
           config.pdf.bounding_box([rect.left+2, rect.top], :width => rect.width - 2) do
             config.pdf.font "Helvetica", :size => 10, :style => :bold
             config.pdf.move_down 3
@@ -109,14 +115,20 @@ module TTV
             config.pdf.text @item.question, :leading => 1 # question
             rect.top -= config.pdf.bounds.height
           end
-          draw_checkbox config, rect, "Yes"
-          draw_checkbox config, rect, "No"
+          rect.top -= 3
+          config.draw_checkbox rect, "Yes"
+          rect.top -= 3
+          config.draw_checkbox  rect, "No"
+          config.pdf.line_width 0.5
+          rect.top -= 3
           config.pdf.stroke_line([rect.left, rect.top], [rect.right, rect.top])
+          config.pdf.stroke_line [rect.right, rect.top], [rect.right, top]
         end
       end
 
       class Contest < FlowItem
         def draw_into(config, rect)
+          top = rect.top
           config.pdf.bounding_box([rect.left+2, rect.top], :width => rect.width - 2) do
             config.pdf.font "Helvetica", :size => 10, :style => :bold
             config.pdf.move_down 3
@@ -125,37 +137,38 @@ module TTV
           end
           @item.candidates.each do |candidate|
             rect.top -= 6
-            draw_checkbox config, rect, candidate.display_name + "\n" + candidate.party.display_name
+            config.draw_checkbox rect, candidate.display_name + "\n" + candidate.party.display_name
           end
+          @item.open_seat_count.times do
+              rect.top -= 6
+              left = config.draw_checkbox rect, "or write in"
+              config.pdf.dash 1
+              v = 16
+              config.pdf.stroke_line [rect.left + left, rect.top - v], 
+                                     [rect.right - 6, rect.top - v]
+              rect.top -= v
+              config.pdf.undash
+          end
+          rect.top -= 6 if @item.open_seat_count != 0
+          config.pdf.line_width 0.5
           config.pdf.stroke_line([rect.left, rect.top], [rect.right, rect.top])
+          config.pdf.stroke_line [rect.right, rect.top], [rect.right, top]
         end
       end
 
-      def draw_checkbox(config, rect, text)
-        bbwidth, bbheight = 22, 10
-        config.pdf.bounding_box [rect.left+6, rect.top], :width => rect.width - 2 do
-          config.pdf.line_width 1.5
-          config.pdf.stroke_rectangle [0,0], bbwidth, bbheight
-          config.pdf.line_width 1
-        end
-        spacer = 6 + bbwidth + 6
-        config.pdf.bounding_box [rect.left + spacer, rect.top], :width => rect.width - spacer do
-          config.pdf.font "Helvetica", :size => 10
-          config.pdf.text text
-          rect.top -= [config.pdf.bounds.height, bbheight].max
-        end
-      end
     end
 
     class BallotConfig
       attr_accessor :pdf, :page_size, :left_margin, :right_margin, :top_margin, :bottom_margin, :columns
-      def initialize(style)
-        @style = style
+      def initialize()
         @page_size = "LETTER"
         @left_margin = @right_margin = 18
-        @top_margin = @bottom_margin = 60
+        @top_margin = @bottom_margin = 30
+        @pleaseVoteHeight = 30
         @padding = 8
         @columns = 3
+        Rails.logger.info("My ancestors are #{self.ancestors}")
+        @file_root = "#{RAILS_ROOT}/ballots/#{}/ballot_config.rb"
       end
 
       def setup(pdf, election, precinct)
@@ -170,6 +183,14 @@ module TTV
         #          )
       end
 
+      def load_text(filename)
+        
+      end
+      
+      def load_image(filename)
+        
+      end
+      
       def debug_stroke_bounds #debug version, rainbow of colors
         @stroke_colors = ["FF0000", "00FF00", "0000FF", "FFFF00", "FF00FF", "00FFFF"] unless @stroke_colors
         old = @pdf.stroke_color
@@ -188,29 +209,52 @@ module TTV
         end
       end
 
+      def draw_checkbox(rect, text)
+        bbwidth, bbheight = 22, 10
+        @pdf.bounding_box [rect.left+6, rect.top], :width => rect.width - 2 do
+          @pdf.line_width 1.5
+          @pdf.stroke_rectangle [0,0], bbwidth, bbheight
+          @pdf.line_width 1
+        end
+        spacer = 6 + bbwidth + 6
+        @pdf.bounding_box [rect.left + spacer, rect.top], :width => rect.width - spacer do
+          @pdf.font "Helvetica", :size => 10
+          @pdf.text text
+          rect.top -= [@pdf.bounds.height, bbheight].max
+        end
+        spacer  # returns left-hand side of text position
+      end
+
       def render_frame(flow_rect)
         barWidth = 18
         barHeight = 140
-        @pdf.fill_color = "#000000"
-        @pdf.rectangle([0,barHeight], barWidth, barHeight)
-        @pdf.rectangle(@pdf.bounds.top_left, barWidth, barHeight)
-        @pdf.rectangle([@pdf.bounds.right - barWidth, barHeight], barWidth, barHeight)
-        @pdf.fill_and_stroke
-        @pdf.stroke_rectangle([barWidth + @padding,@pdf.bounds.height], @pdf.bounds.width - (barWidth + @padding)* 2, @pdf.bounds.height)
-        @pdf.font("Courier", :size => 14)
-        @pdf.text "Sample Ballot", :at => [16, 275], :rotate => 90
-        @pdf.text "Sample Ballot", :at => [@pdf.bounds.right - 2 , 275], :rotate => 90
-        @pdf.text "12001040100040", :at => [16, 410], :rotate => 90
-        @pdf.text "132301113", :at => [@pdf.bounds.right - 2, 146], :rotate => 90
-        flow_rect.inset(barWidth + @padding,0)
+        
+        @pdf.bounding_box [0, @pdf.bounds.height - @pleaseVoteHeight], :width => @pdf.bounds.width, :height => @pdf.bounds.height - @pleaseVoteHeight * 2 do
+          @pdf.fill_color = "#000000"
+          @pdf.rectangle [0,barHeight], barWidth, barHeight
+          @pdf.rectangle @pdf.bounds.top_left, barWidth, barHeight
+          @pdf.rectangle [@pdf.bounds.right - barWidth, barHeight], barWidth, barHeight
+          @pdf.fill_and_stroke
+          @pdf.stroke_rectangle [barWidth + @padding,@pdf.bounds.height], 
+                              @pdf.bounds.width - (barWidth + @padding)* 2, @pdf.bounds.height
+          @pdf.font("Courier", :size => 14)
+          @pdf.text "Sample Ballot", :at => [16, 275], :rotate => 90
+          @pdf.text "Sample Ballot", :at => [@pdf.bounds.right - 2 , 275], :rotate => 90
+          @pdf.text "12001040100040", :at => [16, 410], :rotate => 90
+          @pdf.text "132301113", :at => [@pdf.bounds.right - 2, 146], :rotate => 90
+          flow_rect.inset barWidth + @padding, @pleaseVoteHeight
+        end
       end
 
       def render_header(flow_rect)
-        @pdf.font("Helvetica", :size => 13)
-        @pdf.bounding_box([flow_rect.left + @padding, flow_rect.top - @padding / 3], :width => flow_rect.width - @padding) do
+        @pdf.font "Helvetica", :size => 13,  :style => :bold
+        @pdf.bounding_box [flow_rect.left + @padding, flow_rect.top], 
+                          :width => flow_rect.width - @padding do
+          @pdf.move_down 3
           @pdf.text "OFFICIAL BALLOT"
           @pdf.text @election.start_date.strftime("%B %d, %Y")
-          @pdf.bounding_box([@pdf.bounds.width / 3,  @pdf.bounds.height], :width => @pdf.bounds.width * 2 / 3) do
+          @pdf.bounding_box [@pdf.bounds.width / 3,  @pdf.bounds.height], 
+                            :width => @pdf.bounds.width * 2 / 3 do
             @pdf.text @election.display_name, :align => :center
             @pdf.text @precinct.display_name, :align => :center
             @pdf.move_down(@padding / 3)
@@ -218,7 +262,29 @@ module TTV
           end
         end
         @pdf.stroke_color "000000"
-        @pdf.stroke_line [flow_rect.left, flow_rect.height], [flow_rect.right, flow_rect.height]
+        @pdf.stroke_line [flow_rect.left, flow_rect.top], [flow_rect.right, flow_rect.top]
+      end
+
+      def render_column_instructions(flow_rect, page)
+        return if page != 1
+        @pdf.bounding_box [flow_rect.left + @padding, flow_rect.top], 
+                          :width => flow_rect.width - @padding * 2 do
+            @pdf.text
+        end
+        flow_rect.top = flow_rect.bottom
+      end
+
+      def page_complete(pagenum, last_page)
+        if last_page
+          @pdf.font "Helvetica", :size => 14, :style => :bold
+          @pdf.bounding_box [ 0 , @pdf.bounds.height ], :width => @pdf.bounds.width do
+            @pdf.text "Vote Both Sides", :align => :center
+          end
+          @pdf.bounding_box [ 0 , @pleaseVoteHeight ], :width => @pdf.bounds.width do
+            @pdf.move_down 10
+            @pdf.text "Vote Both Sides", :align => :center
+          end
+        end
       end
 
       def create_flow_item(item)
@@ -277,7 +343,7 @@ module TTV
     
       end
 
-      def render_page(number)
+      def render_page(pagenum)
         @pdf.start_new_page
         flow_rect = Rect.create_bound_box(@pdf.bounds)
         @c.render_frame(flow_rect)
@@ -289,7 +355,6 @@ module TTV
           column_width, flow_rect.height)
         end
         0.upto(@c.columns-1) do |x|
-          @pdf.stroke_line [column_rects[x].right, column_rects[x].top],
           [column_rects[x].right, column_rects[x].bottom] 
         end
         col = 0
@@ -306,15 +371,30 @@ module TTV
             break if col == @c.columns
           end
         end
+        @c.page_complete(pagenum, @flow_items.size > 0)
       end
     end
 
     def self.getBallotConfig(style)
-      BallotConfig.new(style)
+      style ||= "default"
+      return BallotConfig.new() if style == "default"
+      
+      name = "#{RAILS_ROOT}/ballots/#{style}/ballot_config.rb"
+      if File.exists? name
+        begin
+          load name
+          c = TTV::PDFBallot.const_get(style.camelize).const_get("BallotConfig")
+          c.new
+        rescue
+          raise "File #{name} has not defined TTV::PDFBallot::#{style.camelize}::BallotConfig "
+        end
+      else
+        raise "Illegal ballot style: file #{name} does not exist."
+      end
     end
 
     def self.create(election, precinct, style='default')
-      renderer = Renderer.new(election, precinct, getBallotConfig(style))
+      renderer = Renderer.new(election, precinct, getBallotConfig('aiga'))
       renderer.render
       renderer.to_s
     end
