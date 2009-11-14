6aba01bc2e50a00881a5dfb44c6d50734a80ac1c
Aleks Totic <a@totic.org>
2009-11-13 19:52:50 -0800

    Separate rendering for different ballot styles

---
+++ ballots/aiga/ballot_config.rb
@@ -0,0 +1,11 @@
+module TTV
+  module PDFBallot
+    module Aiga
+      class BallotConfig < TTV::PDFBallot::BallotConfig
+        def initialize(style)
+          super
+        end      
+      end
+    end
+  end
+end
