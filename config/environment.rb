d000b216433976b75de189a192512611662b6690
Aleks Totic <a@totic.org>
2010-01-11 20:35:39 -0800

    Downgraded prawn version until nissue 79 with prawn is resolved http://github.com/sandal/prawn/issues/#issue/79

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "authlogic"
-  config.gem "prawn", :version => '>= 0.6.1'
+  config.gem "prawn", :version => '= 0.6.3'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
