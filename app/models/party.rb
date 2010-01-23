26b9d2032647752bb5f0cd730a9430a5747d7d77
Aleks Totic <a@totic.org>
2010-01-22 16:12:04 -0800

    Revert party constant to democrat

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,6 +1,6 @@
 class Party < ActiveRecord::Base
 
-  @@xml_ids = ['american_independent', 'democratic', 'green', 'independent', 'liberitarian', 'peace_and_freedom', 'republican']
+  @@xml_ids = ['american_independent', 'democrat', 'green', 'independent', 'liberitarian', 'peace_and_freedom', 'republican']
 
   def idToXml
     @@xml_ids[self.id]
