12b016f070f20d4df66aac9bcdb6731bdd03b462
Brian Jordan <bcjordan@gmail.com>
2010-04-05 11:53:01 -0400

    New bug: no YAML export of questions

--- test/unit/yaml_xml_equivalency_test.rb
+++ test/unit/yaml_xml_equivalency_test.rb
@@ -10,7 +10,7 @@ require 'yaml'
 class YAMLXMLEquivalencyTest < ActiveSupport::TestCase
   
   # Import XML as @xml_election, exports as YML, imports YML as @yml_election
-  context "Import xml export yml import yml" do
+  context "An XML-imported election and its YAML export / import" do
     setup do
       @file = File.new("test/elections/contests_mix.xml")
 
@@ -43,25 +43,52 @@ class YAMLXMLEquivalencyTest < ActiveSupport::TestCase
       assert_equal @xml_election.display_name, @yaml_election.display_name
     end
     
-    should "contain the same contests" do
+    should "contain the same contests, candidates, parties" do
       @xml_election.contests.each {|xml_contest|
         yaml_contest = @yaml_election.contests.find_by_display_name(xml_contest.display_name)
         assert yaml_contest
         
-        # contain the same candidates
+        # contain the same candidates associated with the same party
         xml_contest.candidates.each {|xml_candidate|
           yaml_candidate = yaml_contest.candidates.find_by_display_name(xml_candidate.display_name)
           assert yaml_candidate
 
-          #puts "XML xml_candidate: " + xml_candidate.display_name + " party " + xml_candidate.party.display_name
-          #puts "YAML yaml_candidate: " + yaml_candidate.display_name + " party " + yaml_candidate.party.display_name
-          
+          # puts "XML xml_candidate: " + xml_candidate.display_name + " party " + xml_candidate.party.display_name
+          # puts "YAML yaml_candidate: " + yaml_candidate.display_name + " party " + yaml_candidate.party.display_name
+          # Test failure is in YAML export of parties -- not exporting party_display_name  
           assert_equal yaml_candidate.party.display_name, xml_candidate.party.display_name
-          
         }
       }
     end
     
+    should "contain the same districts and precincts" do
+      @xml_election.districts.each {|xml_district|
+        yaml_district = @yaml_election.districts.find_by_display_name(xml_district.display_name)
+        assert yaml_district
+        
+        # is the same district type
+        assert_equal xml_district.district_type, yaml_district.district_type
+        
+        # contain the same precincts
+        xml_district.precincts.each {|xml_precinct|
+          yaml_precinct = yaml_district.precincts.find_by_display_name(xml_precinct.display_name)
+          assert yaml_precinct
+        }
+      }
+    end
+    
+    should "contain the same questions" do
+      @xml_election.questions.each {|xml_question|
+        yaml_question = @yaml_election.questions.find_by_display_name(xml_question.display_name)
+        assert yaml_question
+        
+        assert_equal xml_question.question, yaml_question.question
+        
+        # TODO: check for district equivalence
+        #assert_equal xml_question.district, yaml_question.district
+      }
+      
+    end
     
     should "import should get an election of the right name" do
       assert_equal "contests_mix.xml", @yaml_election.display_name
