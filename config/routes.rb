0f2bc538b1f98882c41efb2cb9ffde1267588dee
Pito Salas <rps@salas.com>
2010-04-09 15:46:21 -0400

    [FIXING] More connections being made

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ ActionController::Routing::Routes.draw do |map|
       districts.resources :questions, :only => [:new]
     end
     elections.resources :precincts, :member => { :ballot => :get }, :only => []
+    elections.resources :contests
   end
   map.resources :parties
   map.resources :precincts
