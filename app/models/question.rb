00f1eeb06b117f2e3d96e1cb2c01789fd3c0d3fc
Tom Dyer <tdyer1@gmail.com>
2010-04-15 03:52:10 -0400

    added model methods for getting questions requested by a district in one elections precinct

--- app/models/question.rb
+++ app/models/question.rb
@@ -13,7 +13,7 @@
 #
 
 class Question < ActiveRecord::Base
-
+  
   belongs_to  :requesting_district, :class_name => 'District'
   belongs_to :election
 
@@ -25,4 +25,12 @@ class Question < ActiveRecord::Base
   def to_s
     "QUESTION: #{self.display_name}"
   end
+
+  def self.questions_for_precinct_election(p, e)
+    d = p.districts_for_election(e)
+    e.questions.map do |q|
+      q if d.include?(q.requesting_district)      
+    end.flatten.compact
+  end
+  
 end
