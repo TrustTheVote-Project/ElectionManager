20567c9301ef5500557b5c3f02297ad983b8ff13
Tom Dyer <tdyer1@gmail.com>
2010-04-20 17:03:04 -0400

    added validations for user role names

--- test/unit/user_role_test.rb
+++ test/unit/user_role_test.rb
@@ -2,9 +2,26 @@ require 'test_helper'
 
 class UserRoleTest < ActiveSupport::TestCase
   context "User Role" do
+    
+    context "with an invalid role name" do
+      
+      should "assert a bad value" do
+        assert_bad_value(UserRole, :name, "foo", "Invalid Role")
+      end
+    end
+    
+    %w{ root standard public}.each do |role_name|
+      context "with a value role name of #{role_name}" do
+
+        should "assert a good value" do
+          assert_good_value(UserRole, :name, role_name)
+        end
+      end
+    end
+    
     context "without a user " do
       setup do
-        @role = UserRole.new(:name => "role1")        
+        @role = UserRole.new(:name => "public")        
       end
       
       should "save" do
@@ -17,7 +34,7 @@ class UserRoleTest < ActiveSupport::TestCase
     context "with a user" do
       
       setup do
-        role = UserRole.new(:name => "role1")
+        role = UserRole.new(:name => "public")
         role.user =  User.new(:email => "user1@example.com", :password => "password1", :password_confirmation => "password1")
         role.save
         
@@ -40,7 +57,7 @@ class UserRoleTest < ActiveSupport::TestCase
   
   context "User with roles" do
     setup do
-      role = UserRole.new(:name => "role1")
+      role = UserRole.new(:name => "public")
       role.user =  User.new(:email => "user1@example.com", :password => "password1", :password_confirmation => "password1")
       role.save
     end
@@ -52,7 +69,7 @@ class UserRoleTest < ActiveSupport::TestCase
     end
 
     should "have the correct role name" do
-      assert subject.role?("role1")
+      assert subject.role?("public")
       assert subject.role?(UserRole.last.name)
     end
     
