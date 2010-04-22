8ded96f780a14c2e49b943089451581fbf03998a
Pito Salas <rps@salas.com>
2010-04-22 10:04:19 -0400

    [COMPLETE] First round of new jurisdiction state

--- app/controllers/elections_controller.rb
+++ app/controllers/elections_controller.rb
@@ -4,12 +4,13 @@ class ElectionsController < ApplicationController
     @elections = Election.paginate(:per_page => 10, :page => params[:page])
     redirect_to :action => 'new' if @elections.empty?
     @election = UserSession.find.election if UserSession.find
+    render :action => :jurisd_index
   end
   
   def jurisdiction_index
     @jurisdiction = session[:jurisdiction]
     if @jurisdiction.nil?
-      redirect_to :action => :set_jurisdiction
+      redirect_to :action => :change_jurisdiction
     else
       @old_jurisd = @jurisdiction.id
       @elections = DistrictSet.find(@old_jurisd).elections.paginate(:per_page => 10, :page => params[:page])
