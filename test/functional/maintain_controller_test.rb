5d9961d2bd683151538c67b8d2f29b47c85b51e2
J. Gray <jeffreygray@me.com>
2010-04-27 11:21:15 -0400

    Fixing maintain_controller_test

--- test/functional/maintain_controller_test.rb
+++ test/functional/maintain_controller_test.rb
@@ -4,12 +4,12 @@ class MaintainControllerTest < ActionController::TestCase
   
   test "should import batch yml" do
     get(:import_batch, {'import_folder_path' => 'test/elections/','commit' => 'YML files'})
-    assert_equal 'Election import was successful. Here is your new election.', flash[:notice]
+    assert_equal 'Election import was successful. Here are your elections', flash[:notice]
   end
   
   test "should import batch xml" do
     get(:import_batch, {'import_folder_path' => 'test/elections/','commit' => 'XML files'})
-    assert_equal 'Election import was successful. Here is your new election.', flash[:notice]
+    assert_equal 'Election import was successful. Here are your elections', flash[:notice]
   end
   
 end
