55f0ed8e2995911682eb34d9489d69c564a9b052
J. Gray <jeffreygray@me.com>
2010-04-27 12:09:21 -0400

    Fixing merge conflicts

--- app/controllers/maintain_controller.rb
+++ app/controllers/maintain_controller.rb
@@ -25,7 +25,7 @@ class MaintainController < ApplicationController
   end  
   
   def import_batch
-    begin
+    #begin
       if params[:import_folder_path].length == 0 
         flash[:error] = "Import failed because the directory was not specified."
         redirect_to :back
@@ -39,12 +39,12 @@ class MaintainController < ApplicationController
       end
       flash[:notice] = "Election import was successful. Here are your elections"
       redirect_to elections_url
-    rescue ActionController::RedirectBackError => ex
-      redirect_to elections_url
-    rescue Exception => ex
-      flash[:error] = "Import error: #{ex.message}";
-      redirect_to elections_url
-    end
+    # rescue ActionController::RedirectBackError => ex
+    #   redirect_to elections_url
+    # rescue Exception => ex
+    #   flash[:error] = "Import error: #{ex.message}";
+    #   redirect_to elections_url
+    # end
   end
 
   def export_file       
