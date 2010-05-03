5a00813e57050292e27b1b57e41ecf8b068c04d5
Brian Jordan <bcjordan@gmail.com>
2010-05-03 13:47:45 -0400

    Add unit test for district set

--- test/unit/district_set_test.rb
+++ test/unit/district_set_test.rb
@@ -1,4 +1,18 @@
 require 'test_helper'
 
 class DistrictSetTest < ActiveSupport::TestCase
+    context 'initialization' do
+    
+      should 'be able to create a district set' do
+  
+        district_set = DistrictSet.new(:display_name => "District Set", :secondary_name => "It's a district!")
+        
+        assert district_set.save
+        assert_equal "District Set", district_set.display_name
+        assert_equal "It's a district!", district_set.secondary_name
+      end
+      
+      should_have_attached_file :icon
+    
+  end
 end
