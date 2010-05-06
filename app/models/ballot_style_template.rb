60c4f51aa626b1b469cbd389794c540c9d240deb
J. Gray <jeffreygray@me.com>
2010-05-06 14:32:26 -0400

    [CHANGE] Removed state_graphic property from model and views
    [CHANGE] Remove state_seal property from model and views
    [NEW] Setup paperclip to upload PDF to BallotStyleTemplate model.
    [CHANGE] Changed views to accommodate new paperclip upload

--- app/models/ballot_style_template.rb
+++ app/models/ballot_style_template.rb
@@ -1,3 +1,11 @@
 class BallotStyleTemplate < ActiveRecord::Base
-  validates_presence_of [:display_name,:instruction_text,:state_graphic], :on => :create, :message => "can't be blank"
+  validates_presence_of [:display_name], :on => :create, :message => "can't be blank"
+  
+  has_attached_file :instructions_pdf,
+    :styles => {
+      :thumb=> "100x100#",
+      :small  => "150x150>"
+      # :medium => "300x300>",
+      #       :large =>   "400x400>" 
+      }
 end
