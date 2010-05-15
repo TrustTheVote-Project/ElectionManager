ca28c0053b71a3b07d6259af0ae3e8b2daacc512
Tom Dyer <tdyer1@gmail.com>
2010-05-15 00:19:12 -0400

    added "standard" user role access for elections

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,16 +3,20 @@ class Ability
   include CanCan::Ability
   
   def initialize(user)
+    
     unless user
       user =  User.new
       user.roles << UserRole.new(:name => 'public')
     end
     
-     if user.role? :root 
+    if user.role?(:root)
        can :manage, :all
-     else
+     elsif user.role?(:standard)
+      can [:manage], [Election]
+      # can [:read, :create, :update, :destroy], [Election]
+      # can [:index], [Election]
+    else
        can :read, :all
      end
-    
   end
 end
