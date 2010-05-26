5b0cb80786fb3b92d47c60f5f7d0bd4b4bdf57fe
Tom Dyer <tdyer1@gmail.com>
2010-05-26 09:54:19 -0400

    added new steps

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -35,18 +35,26 @@ Given /^[U|u]sers with the following roles$/ do |table|
   end
 end
 
+#Given /^I am a "([^\"]*)" user with "([^\"]*)"$/ do |role, email|
+Given /^I am a user with a role of "([^\"]*)" and an email "([^\"]*)"$/ do |role, email|
+  login(:email => email,:role => role)
+end
+
+
 Given /^I am logged in as one of the following users$/ do |table|
   table.hashes.each do |hash|
     login({:email => hash['email'], :password => hash['password'],:password_confirmation => hash['password'], :role => hash['role']})
   end
 end
 
-Then /^I should see the users email within "([^\"]*)"$/ do |selector|
-  within(selector) do |content|
-      hc = Webrat::Matchers::HasContent.new(controller.send(:current_user).email)
-      assert hc.matches?(content), hc.failure_message
-  end
-end
+# Then /^I should see the users email within "([^\"]*)"$/ do |selector|
+#    puts "TGD: current_user = #{controller.send(:current_user).email}"
+
+#   within(selector) do |content|
+#       hc = Webrat::Matchers::HasContent.new(controller.send(:current_user).email)
+#       assert hc.matches?(content), hc.failure_message
+#   end
+# end
 
 Then /^I should see the users jurisdiction within "([^\"]*)"$/ do |selector|
   within(selector) do |content|
