169bf9418dfc267d511a9c56eb5f48fd2dcfde3b
Brian Jordan <bcjordan@gmail.com>
2010-05-28 11:13:12 -0400

    Don't need default routes anymore

--- config/routes.rb
+++ config/routes.rb
@@ -80,5 +80,5 @@ ActionController::Routing::Routes.draw do |map|
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
 #  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+#  map.connect ':controller/:action/:id.:format'
 end
