3d80e18238173b75b20b770e271130e633c3ba35
Tom Dyer <tdyer1@gmail.com>
2010-06-15 19:54:42 -0400

    updated functional tests for users to check the access control for public users

--- test/functional/users_mock_controller_test.rb
+++ test/functional/users_mock_controller_test.rb
@@ -1,29 +1,31 @@
 require 'test_helper'
 
 class UsersMockControllerTest < ActionController::TestCase
-  context "without a logged in user" do
-    
-    setup do
-      @controller = UsersController.new
-    end
-    
-    # Allow guest/public users to get the registration page.
-    context "on GET to :new" do    
+  self.login_as(:roles => %w{public}) do
+    context "without a logged in user" do
+      
       setup do
-        tmp_user = User.new
-        User.expects(:new).returns(tmp_user)
-        get :new
+        @controller = UsersController.new
       end
       
-      should_assign_to :user
-      should_respond_with :success
-      should_render_template :new
-      should_not_set_the_flash
-      
-      should "create an unsaved user" do
-        assert assigns(:user).new_record?
-      end
-    end # NEW ACTION
-  end # END tests for public, not logged_in, users
+      # Allow guest/public users to get the registration page.
+      context "on GET to :new" do    
+        setup do
+          tmp_user = User.new
+          User.expects(:new).returns(tmp_user)
+          get :new
+        end
+        
+        should_assign_to :user
+        should_respond_with :success
+        should_render_template :new
+        should_not_set_the_flash
+        
+        should "create an unsaved user" do
+          assert assigns(:user).new_record?
+        end
+      end # NEW ACTION
+    end # END tests for public, not logged_in, users
 
+  end
 end
