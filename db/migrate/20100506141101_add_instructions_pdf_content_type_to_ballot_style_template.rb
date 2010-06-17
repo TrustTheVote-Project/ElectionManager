727ef5a7bad1b8e1d750ff3b0551f786581007bc
Brian Jordan <bcjordan@gmail.com>
2010-06-17 17:34:38 -0400

    [FIX] Revert to original migration command

--- db/migrate/20100506141101_add_instructions_pdf_content_type_to_ballot_style_template.rb
+++ db/migrate/20100506141101_add_instructions_pdf_content_type_to_ballot_style_template.rb
@@ -1,9 +1,9 @@
 class AddInstructionsPdfContentTypeToBallotStyleTemplate < ActiveRecord::Migration
   def self.up
-    add_column :ballot_style_templates, :instructions_image_content_type, :string
+    add_column :ballot_style_templates, :instructions_pdf_content_type, :string
   end
 
   def self.down
-    remove_column :ballot_style_templates, :instructions_image_content_type
+    remove_column :ballot_style_templates, :instructions_pdf_content_type
   end
 end
