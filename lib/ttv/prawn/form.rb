d128e661b3ae427c4126f133b58690df4f6d477c
Tom Dyer <tdyer1@gmail.com>
2010-07-21 18:29:47 -0400

    more work on pdf form text fields and DSL

--- lib/ttv/prawn/form.rb
+++ lib/ttv/prawn/form.rb
@@ -2,18 +2,22 @@ module TTV
   module Prawn
     module Form
       
+      attr_reader :resources, :fields
+      
       def test_method
         "This worked"
       end
       
-      def form(options={} )
-        # state.store.root.data[:AcroForm] ||= state.store.ref({:DR => acroform_resources,:DA => Prawn::Core::LiteralString.new("/Helv 0 Tf 0 g"),:Fields => []})
+      def form(options={}, &block )
+
+        @fields = []
+        instance_eval(&block) if block_given?
+        
         data = state.store.root.data
-        data[:AcroForm] = state.store.ref({:Fields => []})
-        
-        data[:AcroForm][:DA] = ::Prawn::Core::LiteralString.new("/Helv 0 Tf 0 g") unless data[:AcroForm][:DA]
-        
-        resources unless data[:AcroForm][:DR]
+        data[:AcroForm] = state.store.ref(:Fields => (@fields || fields),
+                                          :DR => (@resources || resources),
+                                          :DA => ::Prawn::Core::LiteralString.new("/Helv 0 Tf 0 g")
+                                          )
       end
       
       def resources(options={})
@@ -21,8 +25,12 @@ module TTV
           :Subtype  => :Type1,
           :BaseFont => :Helvetica,
           :Encoding => :WinAnsiEncoding }.merge(options)
-        helv = ref(options)
-        state.store.root.data[:AcroForm][:DR] = ref(:Font => {:Helv => helv})
+        @resources = ref(options)
+      end
+      
+      def fields(&block)
+        instance_eval(&block) if block_given?
+        @fields ||= []
       end
       
       def text_field(name, x, y, w,h, opts = { })
@@ -39,6 +47,49 @@ module TTV
         end
         
         add_interactive_field(:Tx, field_dict)
+      end # text_field
+
+      def text_field2(name, x, y, w,h, opts = { })
+        field_dict = {
+          # type of field is a text field
+          :FT => :Tx,
+          # partial field name
+          :T => ::Prawn::Core::LiteralString.new(name),
+          # default appearance, font, size, color, and so forth
+          :DA => ::Prawn::Core::LiteralString.new("/Helv 0 Tf 0 g"),
+          # Not sure what this is? Maybe the file specification?
+          :F => 4,
+          # field flag, not read only, not required, can be exported
+          :Ff => 0,
+        }
+        
+        if opts[:default]
+          # field's value
+          field_dict[:V] = ::Prawn::Core::LiteralString.new(opts[:default])
+        end
+        
+        # The PDF object for this text box can also be used as
+        # Annotation dictionary.
+        # If any form field only has one annotation then it can be
+        # used to represent both a field and and annotation      
+        # sect 8.4.5 Widget Annotations
+        annotation_dict = {
+          :Type => :Annot,
+          # This is a Widget annotation
+          :Subtype => :Widget,
+          # MK is the appearance character dictionary
+          # BC is the widget annotation's border color, (DeviceRGB)
+          :MK => {:BC => [0, 0, 0]},
+          # BS is the border style dictionary,(width and dash pattern)
+          # :W => 1 (width 1 point), :S => :S (solid), 
+          :BS => {:Type => :Border, :W => 1, :S => :S},
+          # n rectangle, defining the location of the annotation on
+          # the page in default userspace units.
+          :Rect => [x, y, x + w, y + h],
+        }
+        
+        @fields << ref(field_dict.merge(annotation_dict))
+        #add_interactive_field(:Tx, field_dict)
       end # text_field
 
 
