382211246fad49b97a73d7a7ca2b7da02b60d846
Tom Dyer <tdyer1@gmail.com>
2010-07-25 22:43:26 -0400

    added references/indirect objects to annotations and fields

--- lib/ttv/prawn/form.rb
+++ lib/ttv/prawn/form.rb
@@ -15,8 +15,7 @@ module TTV
         
         data = state.store.root.data
         data[:AcroForm] = state.store.ref(:Fields => (@fields || fields),
-                                          :DR => (@resources || resources),
-                                          :DA => ::Prawn::Core::LiteralString.new("/Helv 0 Tf 0 g")
+                                          :DR => (@resources || resources)
                                           )
       end
       
@@ -27,45 +26,25 @@ module TTV
           :Encoding => :WinAnsiEncoding }.merge(options)
         @resources = ref(options)
       end
-      
-      def fields(&block)
-        instance_eval(&block) if block_given?
-        @fields ||= []
-      end
-      
-      def text_field(name, x, y, w,h, opts = { })
-        field_dict = {:T => ::Prawn::Core::LiteralString.new(name),
-          :DA => ::Prawn::Core::LiteralString.new("/Helv 0 Tf 0 g"),
-          :F => 4,
-          :Ff => 0,
-          :BS => {:Type => :Border, :W => 1, :S => :S},
-          :MK => {:BC => [0, 0, 0]},
-          :Rect => [x, y, x + w, y + h]}
-        
-        if opts[:default]
-          field_dict[:V] = ::Prawn::Core::LiteralString.new(opts[:default])
-        end
-        
-        add_interactive_field(:Tx, field_dict)
-      end # text_field
 
-      def text_field2(name, x, y, w,h, opts = { })
+      def draw_text_field(name, opts={}, &block )
+        options = { :width => 100, :height => font.height+font.line_gap}.merge(opts)
+        x,y = map_to_absolute(options[:at])
+        
         field_dict = {
           # type of field is a text field
           :FT => :Tx,
-          # partial field name
+          # (partial) field name
           :T => ::Prawn::Core::LiteralString.new(name),
           # default appearance, font, size, color, and so forth
-          :DA => ::Prawn::Core::LiteralString.new("/Helv 0 Tf 0 g"),
-          # Not sure what this is? Maybe the file specification?
-          :F => 4,
-          # field flag, not read only, not required, can be exported
+          #:DA => ::Prawn::Core::LiteralString.new("/Helv 0 Tf 0 g"),
+          # field flag: not read only, not required, can be exported
           :Ff => 0,
         }
         
-        if opts[:default]
+        if options[:default]
           # field's value
-          field_dict[:V] = ::Prawn::Core::LiteralString.new(opts[:default])
+          field_dict[:V] = ::Prawn::Core::LiteralString.new(options[:default])
         end
         
         # The PDF object for this text box can also be used as
@@ -77,19 +56,35 @@ module TTV
           :Type => :Annot,
           # This is a Widget annotation
           :Subtype => :Widget,
+          # Rectangle, defining the location of the annotation on
+          # the page in default userspace units.
+          :Rect => [x, y, x + options[:width] , y + options[:height]],
+          # Annotation Flag. see 8.4.2 Annotation Flags
+          # not invisible, not hidden, print annotation when page is printed,...
+          :F => 4,
+          # :Contents => "Some contents here",
           # MK is the appearance character dictionary
           # BC is the widget annotation's border color, (DeviceRGB)
-          :MK => {:BC => [0, 0, 0]},
+          #:MK => {:BC => [0, 0, 0]},
           # BS is the border style dictionary,(width and dash pattern)
           # :W => 1 (width 1 point), :S => :S (solid), 
           :BS => {:Type => :Border, :W => 1, :S => :S},
-          # n rectangle, defining the location of the annotation on
-          # the page in default userspace units.
-          :Rect => [x, y, x + w, y + h],
         }
+
+        dict = field_dict.merge(annotation_dict)
+
+        # allow one to add to the dictionary in the block
+        yield dict  if block_given?
         
-        @fields << ref(field_dict.merge(annotation_dict))
-        #add_interactive_field(:Tx, field_dict)
+#        puts "TGD: state.store.ref(dict).to_s = #{state.store.ref(dict).to_s.inspect}"
+        dict_ref = state.store.ref(dict)
+        @fields << dict_ref
+        
+        # add annotations to the current page
+        page.dictionary.data[:Annots] ||= []
+        page.dictionary.data[:Annots] << dict_ref
+        
+
       end # text_field
 
 
@@ -109,34 +104,6 @@ module TTV
           out << "\n  -------------"
         end
         out
-      end
-
-      private
-      
-      def add_interactive_field(type, opts = {})
-        defaults = {:FT => type, :Type => :Annot, :Subtype => :Widget}
-        #      annotation = @store.ref(opts.merge(defaults))
-        annotation = state.store.ref(opts.merge(defaults))
-        acroform.data[:Fields] << annotation
-        page.dictionary.data[:Annots] ||= []
-        page.dictionary.data[:Annots] << annotation
-      end
-      
-      # The AcroForm dictionary (PDF spec 8.6) for this document. It is
-      # lazily initialized, so that documents that do not use interactive
-      # forms do not incur the additional overhead.
-      def acroform
-        state.store.root.data[:AcroForm] ||= state.store.ref({:DR => acroform_resources,:DA => ::Prawn::Core::LiteralString.new("/Helv 0 Tf 0 g"),:Fields => []})
-      end
-      
-      # a resource dictionary for interactive forms. At a minimum,
-      # must contain the font we want to use
-      def acroform_resources
-        helv = ref(:Type     => :Font,
-                   :Subtype  => :Type1,
-                   :BaseFont => :Helvetica,
-                   :Encoding => :WinAnsiEncoding)
-        ref(:Font => {:Helv => helv})
       end
 
     end # Form
