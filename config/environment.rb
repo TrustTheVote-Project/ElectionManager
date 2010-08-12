0ff3de4e617d15af3751d06b876fc2d149e99a8e
Tom Dyer <tdyer1@gmail.com>
2010-08-12 10:46:50 -0400

    removed warning in 2.3.8 about the session key name

--- config/environment.rb
+++ config/environment.rb
@@ -53,7 +53,7 @@ Rails::Initializer.run do |config|
   
   # session in a cookie config
   config.action_controller.session = { 
-    :session_key => 'TrustTheVote', 
+    :key => 'TrustTheVote', 
     :secret      => '22acc6b566d2328b8d775d1ea25daef6814918c2216c90d292c09e7db2d76bba4285c44607f7d051a1255e1757a9639660d595130c0a05a72ceb23c60c9c5750' 
   }
 end
