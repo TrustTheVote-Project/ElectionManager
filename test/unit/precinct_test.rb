b3af11b58fc1824f542575e6c01b151622c8b9d6
Pito Salas <rps@salas.com>
2010-08-17 12:15:54 -0400

    Remove many to many direct between precincts and districtsetss

--- test/unit/precinct_test.rb
+++ test/unit/precinct_test.rb
@@ -2,36 +2,36 @@ require 'test_helper'
 
 class PrecinctTest < ActiveSupport::TestCase
   context "basic test" do
-
+    
     setup do
       Precinct.create(:display_name => "precinct1")
     end
-
+    
     subject { Precinct.last}
     should_create :precinct
     should_change("the number of precincts", :by => 1) { Precinct.count}    
-    should_have_and_belong_to_many :districts
+    #    should_have_and_belong_to_many :districts
     should_have_class_methods :find
     should_have_instance_methods :before_validation, :districts_for_election
     should_have_db_columns :display_name, :ident
     should_have_attr_reader :display_name
     
-    # doesn't work ???
-    #should_ensure_length_is(:display_name, "precinct1".size)
+    should_ensure_length_is(:display_name, "precinct1".size)
     should "validate length of " do assert_equal subject.display_name.size, "precinct1".size end
     should "have an identity" do
       assert !subject.ident.blank?
     end
     
   end
-
+  
   setup_precincts do
-    subject { Precinct.find_by_display_name "Precint 1"}
-
+    subject { Precinct.find_by_display_name "Precinct 1"}
+    puts subject.inspect
+    
     should "have a couple of districts" do
-      assert_equal 4,  subject.districts.count
-      (0..3).each do |i|
-        assert_equal "District #{i}", subject.districts.find_by_display_name("District #{i}").display_name
+      assert_equal 4,  subject.precinct_splits[0].district_set.districts.count
+       (0..3).each do |i|
+        assert_equal "District #{i}", subject.precinct_split[0].district_set.districts.find_by_display_name("District #{i}").display_name
       end
     end
   end
