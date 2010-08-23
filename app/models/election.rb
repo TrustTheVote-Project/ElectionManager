c4b9aeffa0e62b98063e8c4949c98c9f339c6875
Brian Jordan <bcjordan@gmail.com>
2010-08-23 12:02:19 -0400

    Export XML, export coverage

--- app/models/election.rb
+++ app/models/election.rb
@@ -19,7 +19,7 @@ class Election < ActiveRecord::Base
     has_many :contests, :order => :position, :dependent => :destroy
     has_many :questions, :order => :display_name, :dependent => :destroy
     
-    attr_accessible :ident, :display_name, :district_set_id, :district_set
+    attr_accessible :ident, :display_name, :district_set_id, :district_set, :start_date, :district_set
     
     validates_presence_of :display_name
     belongs_to :district_set
