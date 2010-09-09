f36955cc26bb2a76cafb29cd2498a8a2beb1a0cf
Tom Dyer <tdyer1@gmail.com>
2010-09-09 17:16:33 -0400

    added contests and candidates

--- db/dc_seeds.rb
+++ db/dc_seeds.rb
@@ -1,72 +1,113 @@
-# %w{ moe larry curly }.each do |name|
-#   User.create(:email => "#{name}@example.com", :password => "password", :password_confirmation => "password")
+# ####################################################
+# # Create DistrictTypes
+# ["(built-in default district type)", "State", "County", "Municipality", "School", "Water", "Fire", "Coastal", "Harbor", "US Congressional", "WARD", "CITYWI", "SMD"].each do |dt_name|
+#   DistrictType.create!(:title => dt_name)
 # end
-moe = User.find_or_create_by_email(:email => "moe@example.com", :password => "password", :password_confirmation => "password")
-larry = User.find_or_create_by_email(:email => "larry@example.com", :password => "password", :password_confirmation => "password")
-curly = User.find_or_create_by_email(:email => "curly@example.com", :password => "password", :password_confirmation => "password")
 
-# create roles
-%w{ public standard root}.each do |rolename|
-  UserRole.find_or_create_by_name(:name => rolename)
-end
+# ####################################################
+# # Create Districts
+# d = District.new( :display_name => "CITY OF WASHINGTON WARD 6" )
+# d.district_type = DistrictType.find_by_title("WARD")
+# d.save!
 
-moe.roles << UserRole.find_by_name('root')
-larry.roles << UserRole.find_by_name('standard')
-curly.roles << UserRole.find_by_name('public')
+# d = District.new( :display_name => "DISTRICT OF COLUMBIA" )
+# d.district_type = DistrictType.find_by_title("CITYWI")
+# d.save!
 
-####################################################
-# Create DistrictTypes
-["(built-in default district type)", "State", "County", "Municipality", "School", "Water", "Fire", "Coastal", "Harbor", "US Congressional", "WARD", "CITYWI", "SMD"].each do |dt_name|
-  DistrictType.create!(:title => dt_name)
-end
+# d = District.new( :display_name => "1ST CONGRESSIONAL DIST-DEM")
+# d.district_type = DistrictType.find_by_title("COND")
+# d.save!
 
-####################################################
-# Create Districts
-d = District.new( :display_name => "CITY OF WASHINGTON WARD 6" )
-d.district_type = DistrictType.find_by_title("WARD")
-d.save!
+# d = District.new( :display_name => "SMD 01-ANC 6C")
+# d.district_type = DistrictType.find_by_title("SMD")
+# d.save!
 
-d = District.new( :display_name => "DISTRICT OF COLUMBIA" )
-d.district_type = DistrictType.find_by_title("CITYWI")
-d.save!
+# d = District.new( :display_name => "SMD 02-ANC 6C")
+# d.district_type = DistrictType.find_by_title("SMD")
+# d.save!
 
-d = District.new( :display_name => "1ST CONGRESSIONAL DIST-DEM")
-d.district_type = DistrictType.find_by_title("COND")
-d.save!
+# d = District.new( :display_name => "SMD 03-ANC 6C")
+# d.district_type = DistrictType.find_by_title("SMD")
+# d.save!
 
-d = District.new( :display_name => "SMD 01-ANC 6C")
-d.district_type = DistrictType.find_by_title("SMD")
-d.save!
+# d = District.new( :display_name => "SMD 09-ANC 6C")
+# d.district_type = DistrictType.find_by_title("SMD")
+# d.save!
 
-d = District.new( :display_name => "SMD 02-ANC 6C")
-d.district_type = DistrictType.find_by_title("SMD")
-d.save!
+# d = District.new( :display_name => " CITY OF WASHINGTON WARD 2")
+# d.district_type = DistrictType.find_by_title("CITYWI")
+# d.save!
 
-d = District.new( :display_name => "SMD 03-ANC 6C")
-d.district_type = DistrictType.find_by_title("SMD")
-d.save!
+# d = District.new( :display_name => "SMD 01-ANC 2A")
+# d.district_type = DistrictType.find_by_title("SMD")
+# d.save!
 
-d = District.new( :display_name => "SMD 09-ANC 6C")
-d.district_type = DistrictType.find_by_title("SMD")
-d.save!
+# d = District.new( :display_name => "SMD 05-ANC 2A")
+# d.district_type = DistrictType.find_by_title("SMD")
+# d.save!
 
-d = District.new( :display_name => " CITY OF WASHINGTON WARD 2")
-d.district_type = DistrictType.find_by_title("CITYWI")
-d.save!
-
-d = District.new( :display_name => "SMD 01-ANC 2A")
-d.district_type = DistrictType.find_by_title("SMD")
-d.save!
-
-d = District.new( :display_name => "SMD 05-ANC 2A")
-d.district_type = DistrictType.find_by_title("SMD")
-d.save!
-
-d = District.new( :display_name => "SMD 06-ANC 2A")
-d.district_type = DistrictType.find_by_title("SMD")
-d.save!
+# d = District.new( :display_name => "SMD 06-ANC 2A")
+# d.district_type = DistrictType.find_by_title("SMD")
+# d.save!
 
 ####################################################
 # Create Jurisdictions
-DistrictSet.create!( "secondary_name"=>"", "descriptive_text"=>"Nation Capitol",  display_name =>"District Of Columbia"  )
+#DistrictSet.create!( "secondary_name"=>"", "descriptive_text"=>"Nation Capitol",  :display_name =>"DC Jurisdiction"  )
+
+####################################################
+# Create Election
+# e = Election.new(:display_name => "DC Election")
+# e.default_voting_method_id = 1
+# e.save!
+
+# !!!!!!!!!!!!
+# 1) create a Jurisdiction named "DC Jurisdiction".
+# 1.1)  make this Jurisdiction the current jurisdiction.
+# 2) import /Users/tom/TrustTheVote/converter/outputs/DC/medium.yml into
+# this jurisdiction
+# (This will create the district sets, districts, precinct splits)
+# 3) Create and election named "DC Election"
+# 4) Make this election's Jurisdiction the "DC Jurisdiciton"
+# 5) Go into the console script/console
+# 6) load 'db/dc_seeds.rb'
+# (This will laod THIS file to populate the contests and candidates
+
+####################################################
+# Create Contests
+def create_contest(pos, name, district_name)
+  c = Contest.new(:position=> pos, :open_seat_count => 1, :display_name =>  name) 
+  c.election = Election.find_by_display_name('DC Election')
+  c.district = District.find_by_display_name(district_name)
+  c.voting_method = VotingMethod::WINNER_TAKE_ALL
+  c.save!
+  c
+end
+
+pos = 0
+
+# for district named "DISTRICT OF COLUMBIA"
+['DELEGATE TO THE U.S. HOUSE OF REPRESENTATIVES', 'MAYOR OF THE DISTRICT OF COLUMBIA', "CHAIRMAN OF THE COUNCIL", "AT-LARGE MEMBER OF THE COUNCIL", "UNITED STATES REPRESENTATIVE"].each do |name|
+  contest = create_contest(pos += 1, name, "DISTRICT OF COLUMBIA")
+  ['Democratic', 'Republican', 'Green'].each do |cand_name|
+    cand = Candidate.new(:display_name => "#{cand_name}_#{contest.id}")
+    cand.party = Party.find_by_display_name(cand_name)
+    cand.contest = contest
+    cand.save!
+  end
+end
+
+pos = 0
+# for district named "CITY OF WASHINGTON WARD 6"
+["MEMBER OF THE COUNCIL WARD SIX", "MEMBER OF STATE BOARD OF EDUCATION WARD SIX", ].each do |name|
+  contest = create_contest(pos += 1, name, "CITY OF WASHINGTON WARD 6")
+  ['Democratic', 'Republican', 'Green'].each do |cand_name|
+    cand = Candidate.new(:display_name => "#{cand_name}_#{contest.id}")
+    cand.party = Party.find_by_display_name(cand_name)
+    cand.contest = contest
+    cand.save!
+  end
+end
+
+
+
 
