b934b1c5ac5177e9b6d02942508094ade8db5249
Tom Dyer <tdyer1@gmail.com>
2010-09-20 14:07:11 -0400

    created va ballot strategy

--- lib/ttv/ballot_layout_strategy/va.rb
+++ lib/ttv/ballot_layout_strategy/va.rb
@@ -3,7 +3,29 @@ module ::TTV
     
     class VA < BallotLayoutStrategy::Base
       
-      def initialize
+      class << self
+        
+        def party_order
+          @party_order ||=  {Party::INDEPENDENT => 0, Party.find_by_display_name('IndependentGreen') => 1, Party::DEMOCRATIC => 2, Party::REPUBLICAN => 3}
+        end
+        
+      end # end class methods
+      
+      def initialize(election=nil, precinct_split=nil)
+        @election = election
+        @precinct_split = precinct_split
+      end
+
+      def candidate_ordering
+        #set the default to indy candidate doesn't have a party
+        
+        return lambda do |c1, c2|
+          c1.party = Party::INDEPENDENT unless c1.party
+          c2.party = Party::INDEPENDENT unless c2.party
+
+          self.class.party_order[c1.party] <=> self.class.party_order[c2.party]
+        end
+
       end
       
     end
