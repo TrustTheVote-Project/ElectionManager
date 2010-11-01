b4e672d7bca543e176759eb61eafc85e75654c63
Pito Salas <rps@salas.com>
2010-11-01 11:59:30 -0400

    Fix use of Paperclip to use the right directories.

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,6 +1,9 @@
 class Asset < ActiveRecord::Base
 #  attr_accessor :asset_file_name, :display_name, :asset_updated_at, :asset_content_type
-  has_attached_file :asset, {:styles => { :medium => "300x300>", :thumb => "100x100>" }}
+  has_attached_file :asset, 
+    {:styles => { :medium => "300x300>", :thumb => "100x100>" },
+    :url => "/assets/:id/:style_:filename",
+    :default_url => "/assets/missing_:style.png"}
 
 # Make sure that ident is not nil. If it is, create a unique one.
   def before_validation
