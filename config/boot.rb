a961208d719e15e8ef95051a6f0fbca2b6c1fb30
Tom Dyer <tdyer1@gmail.com>
2010-11-04 20:32:02 -0400

    added bundler files and config

--- config/boot.rb
+++ config/boot.rb
@@ -99,10 +99,24 @@ module Rails
       end
 
       private
-        def read_environment_rb
-          File.read("#{RAILS_ROOT}/config/environment.rb")
-        end
+      def read_environment_rb
+        File.read("#{RAILS_ROOT}/config/environment.rb")
+      end
     end
+  end
+end
+
+class Rails::Boot
+  def run
+    load_initializer
+
+    Rails::Initializer.class_eval do
+      def load_gems
+        @bundler_loaded ||= Bundler.require :default, Rails.env
+      end
+    end
+
+    Rails::Initializer.run(:set_load_path)
   end
 end
 
